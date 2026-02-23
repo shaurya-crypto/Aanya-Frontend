33ba4d93c6b3ad78600f523a1944f3cd64ea8c1e
shaurya-crypto <shauryaprabhakar097@gmail.com>
2026-02-23 20:32:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
   server: {
     host: "0.0.0.0",
     port: 8080,
-    // Add this line below
-    allowedHosts: ["jamir-unrejectable-kristel.ngrok-free.dev"], 
     hmr: {
       overlay: false,
     },
   },
-  // ... rest of your config
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
